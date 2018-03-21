ff8f5b5ba9cfd11a52d8431d6a9912f4bbcd1eaa
geobaldi <geobaldi@gmail.com>
2018-03-21 14:11:44 +0100

    Generalize the gallery item template
    In preparation for the videos

--- ripiu/djangocms_aoxomoxoa/templatetags/aoxomoxoa_filters.py
+++ ripiu/djangocms_aoxomoxoa/templatetags/aoxomoxoa_filters.py
@@ -5,7 +5,27 @@ register = Library()
 
 @register.filter(name='get_size')
 def get_size(thumb_opt):
-    return '%(w)dx%(h)d' % {
-        'w': thumb_opt.width,
-        'h': thumb_opt.height,
-    }
+    return (thumb_opt.width, thumb_opt.height)
+
+
+@register.filter(name='get_alt')
+def get_alt(instance):
+    if hasattr(instance, 'image'):
+        # cmsplugin_filer_image
+        return instance.alt_text or instance.image.default_alt_text
+    if hasattr(instance, 'picture'):
+        # djangocms_picture
+        if 'alt' in instance.attributes and instance.attributes['alt']:
+            return instance.attributes['alt']
+        return instance.picture.default_alt_text
+    return 'PUPPA!'
+
+
+@register.filter(name='get_caption')
+def get_caption(instance):
+    if hasattr(instance, 'image'):
+        # cmsplugin_filer_image
+        return instance.caption_text or instance.image.default_caption
+    if hasattr(instance, 'picture'):
+        # djangocms_picture
+        return instance.caption_text or instance.picture.default_caption
